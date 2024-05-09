a351c4d2fb2de2e5b8500fff5e17b598afcb2e51
luis901810 <luis901810@gmail.com>
2024-05-09 08:53:00 -0500

    server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
-
+const cors = require("cors")
 const server = express()
+
+server.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"]
+}))
 
 module.exports = server;
