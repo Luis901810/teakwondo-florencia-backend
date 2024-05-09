d66095fc638cd13c075a02c50e7a6e7b4acde34f
luis901810 <luis901810@gmail.com>
2024-05-09 13:57:03 -0500

    depedencias y routers

--- src/server.js
+++ src/server.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const cors = require("cors")
+const morgan = require("morgan")
+const cookieParser = require("cookie-parser")
 const server = express()
+const mainRoutes = require("./routes/mainRoutes")
 
 server.use(cors({
     origin: ["http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
     allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"]
-}))
+}));
+
+server.name = "teakwondo-florencia"
+server.use(express.urlencoded({ extended: this.true, limit: "50mb" }))
+server.use(express.json({ limit: "50mb" }))
+server.use(cookieParser())
+server.use(morgan("dev"))
+server.use(mainRoutes)
+
+server.use( (err, req, res, next) =>{ // eslint-disable-line no-unused-vars
+    const status = err.status || 500
+    const message = err.message || err
+    console.log(err)
+    res.status(status).send(message)
+})
+
 
 module.exports = server;
