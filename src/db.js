a0312c4801a67e506da7cbf407ad047e18208a75
luis901810 <luis901810@gmail.com>
2024-05-10 11:30:18 -0500

    creaciones de handler de user

--- src/db.js
+++ src/db.js
@@ -1,16 +1,6 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-// const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env; 
-
-// const sequelize = new Sequelize(
-//     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-//     {
-//         logging: false,
-//         native: false,
-//     },
-// );
-
 
 const sequelize = new Sequelize(process.env.POSTGRES_URL, {
     dialect: "postgres",
